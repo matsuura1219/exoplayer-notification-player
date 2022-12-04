ba0dd82205b1c38b8a6c30f584fa52bea4b38699
matsuura1219 <matsuurayuki1219@gmail.com>
2022-12-04 16:21:40 +0900

    feat: UI実装

--- app/src/main/java/jp/matsuura/notificationplayerapp/MainActivity.kt
+++ app/src/main/java/jp/matsuura/notificationplayerapp/MainActivity.kt
@@ -2,10 +2,16 @@ package jp.matsuura.notificationplayerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.matsuura.notificationplayerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
